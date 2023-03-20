39d564d30f63e8ba841bd06938fa84e8e9d45a69
Ketan Vasudeva <ketanvasudeva@gmail.com>
2023-03-20 19:32:11 -0400

    added logic for movement and input collection for MBs

--- MarkovBrain.py
+++ MarkovBrain.py
@@ -1,5 +1,6 @@
 import numpy as np
 from Gates import Gates
+import math
 
 class MarkovBrain:
     def __init__(self, num_inputs, num_outputs, num_hidden, num_gates, gates, all_gates = True):
@@ -42,9 +43,67 @@ class MarkovBrain:
         self.location = None
 
     # Perform brain update, including fitness update
-    def brain_update(self):
 
-        return
+    # NOTE: This particular brain update is specific to this simulation. 
+    # Input 1: Local temperature
+    # Input 2: Distance to closest agent
+
+    # Output 1-4: up, down, left, and right movement (in that order)
+    def brain_update(self, cave_map, cave_map_grad, other_agents, this_agent, time_of_day = 'DAY'):
+
+        ## INPUT LAYER ##
+
+        # Local temperature
+        self.inputs[0] = cave_map[this_agent]
+
+        # Distance to closest agent
+        self.inputs[0] = None
+
+        ## LOGIC LAYER ##
+
+
+        ## OUTPUT LAYER ##
+
+        # This section enforces movement conditions, including map limits
+        
+        curr_y = this_agent[0]
+        curr_x = this_agent[1]
+
+        # Move up
+        if self.outputs[0] >= 1 and this_agent[0] != 0:
+            curr_y = curr_y - 1
+
+        # Move down
+        if self.outputs[1] >= 1 and this_agent[0] != cave_map.shape[0] - 1:
+            curr_y = curr_y + 1
+
+        # Move left 
+        if self.outputs[2] >= 1 and this_agent[1] != 0:
+            curr_x = curr_x - 1
+        
+        # Move right
+        if self.outputs[3] >= 1 and this_agent[1] != cave_map.shape[1] - 1:
+            curr_x = curr_x + 1
+
+        self.location = (curr_y, curr_x)
+
+        ## UPDATE FITNESS ##
+
+        # Fitness rule : promote temperature gradients
+        if time_of_day == 'DAY':
+            # Promote negative gradients (lower temperatures in caves)
+            self.fitness = self.fitness - cave_map_grad[0][this_agent] - cave_map_grad[1][this_agent] # y and x derivatives
+        else:
+            # Promote positive gradients (higher temperatures in caves)
+            self.fitness = self.fitness + cave_map_grad[0][this_agent] + cave_map_grad[1][this_agent] # y and x derivatives
+
+        # Fitness rule : promote against agents crashing (maintain separation)
+        for agent in other_agents:
+            d = math.dist(agent, this_agent)
+            if d < 2: # covers all locations directly around the current location
+                self.fitness = self.fitness - 30 # harsh penalty if there is a crash
+        
+        return self.location, self.fitness # return the new position of the agent
     
     # Optional: encode as genome
     def encode(self):
--- run_sim.py
+++ run_sim.py
@@ -22,6 +22,10 @@ def selection(brains):
 
     return best_brains
 
+def simulate_step(brain, brain_locations, map):
+    
+    return True
+
 if __name__ == '__main__':
 
     ## Open Configuration File ##
@@ -36,14 +40,18 @@ if __name__ == '__main__':
 
     # Create map
     cave_map = np.loadtxt(params['maps'][0])
+    center = tuple(i//2 for i in cave_map.shape) # extract center coordinate
+    cave_map_grad = np.gradient(cave_map) # pre-compute gradient of cave_map
 
     ## GENERATE FIRST GENERATION ##
 
     agents = []
+    fitness = np.zeros(params['pool_size'])
     # Initialize first pool of Markov Brains
     for agent in range(0, params['pool_size']):
         agents = agents + [MarkovBrain(params['num_inputs'], params['num_outputs'], params['num_hidden'], params['num_gates'], params['gate_types'])]
 
+    '''
     print(agents[0].ids)
     for i in range(0, agents[0].num_gates):
         print(agents[0].gates[i].gate_type)
@@ -54,24 +62,59 @@ if __name__ == '__main__':
         print(agents[0].gates[i].output_prob)
         print(agents[0].gates[i].input_connections)
         print(agents[0].gates[i].output_connections)
+    '''
 
     ## EVOLUTION PROCESS ## 
 
     for evo_step in range(0, params['evolution_steps']):
 
         # This loop goes over every agent variation for this evolution step
-        for agent in agents:
+        for agent_index, agent in enumerate(agents):
 
             # Create simulation pool of agents
             sim_agents = [] # stores copies of the simulation agent
+            agent_locations = []
             for i in range(0, params['swarm_size']):
                 sim_agents = sim_agents + [copy.deepcopy(agent)]
+                loc = None # will be tuple containing location of agent on map
+
+
+            # Initialize locations in a circle around center of map
+            rad = len(sim_agents) // 2 # define radius as half the number of agents to allow for some margin
+
+            # initialize agent locations equidistant around a circle
+            for index in range(len(sim_agents)):
+                agent_locations.append((center[0] + int(np.round(rad*np.cos((index*2*np.pi)/len(sim_agents)))), center[1] + int(np.round(rad*np.sin((index*2*np.pi)/len(sim_agents))))))          
         
             ## SIMULATION PROCESS ##
+
+            sim_fitness = np.zeros(params['swarm_size']) # Store fitness of each agent
+
+            # Simulate over a certain number of time steps
             for sim_step in range(0, params['time_steps']):
+                new_locs = []
 
-                # Complete the simulation process (brain updates, movement, etc.)
+                # Each simulation agent must be updated, all locations are only updated after all updates (parallel updates)
+                for i, sim_agent in enumerate(sim_agents):
+                    
+                    # Identify the locations of other agents
+                    other_agents = agent_locations[:i] + agent_locations[i+1:]
 
+                    this_agent = agent_locations[i] # extract this agent's location
+
+
+                    # Simulates 1 update step and returns the new location for the simulation agent
+                    loc, fit = sim_agent.brain_update(cave_map, cave_map_grad, other_agents, this_agent)
+                    print(sim_agent.location)
+                    new_locs = new_locs + [loc]  
+                    sim_fitness[i] = fit
+
+                # Update agent locations
+                print(agent_locations)
+                agent_locations = list(new_locs)
+
+                print(new_locs)
+                exit()
                 pass
 
         # After all agents have been simulated, compare fitness and evolve
